a12d51fe4fdd852143efafdd8e3d608d55156d20
kevinFernandez <kevinfernandez@gmail.com>
2025-03-05 15:00:51 -0500

    feat: agregar metodos en el controllador de cliente y rutas de cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ClienteRequest;
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+    //Funcion del index
+    public function index(){
+        
+        $clientes = Cliente::all();
+        
+        return view('clientes.index', compact('clientes')); //compact = 'clientes' => $clientes
+    }
+
+    //Vista del formulario
+    public function create(){
+        return view('clientes.create');
+    }
+
+
+    //Funcion para el registro de un cliente
+    public function store(ClienteRequest $request){
+
+        Cliente::created($request->validate());
+
+        return redirect()->route('clientes.index')->with('success', 'Registro realizado exitosamente');
+    }
+
+    
     
 }
