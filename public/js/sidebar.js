3b9094460b0a84eeabb4e7e333493edb91b17765
kevinFernandez <kevinfernandez.peinado@gmail.com>
2025-04-07 19:59:10 -0500

    style: Cambios de estilos en el sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,14 +1,24 @@
+
+//Script para expandir y contraer los modulos del sidebar
 document.addEventListener('DOMContentLoaded', function () {
     const toggles = document.querySelectorAll('.accordion-toggle');
 
     toggles.forEach(toggle => {
         toggle.addEventListener('click', function () {
             const content = this.nextElementSibling;
-            content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+
+            if (content.classList.contains('open')) {
+                content.style.maxHeight = null;
+                content.classList.remove('open');
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                content.classList.add('open');
+            }
         });
     });
 });
 
+//Script para contraer y expander el sidebar
 document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementById('sidebar');
     const toggle = document.getElementById('toggleSidebar');
